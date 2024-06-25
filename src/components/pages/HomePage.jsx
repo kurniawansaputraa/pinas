5cb45a8eb4643e05da566c2ac2673ba0ae5f2fd3
kurniawansaputraa <nebula.stars3122@gmail.com>
2024-06-25 20:43:39 +0700

    update kata kata

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -5,8 +5,15 @@ import './HomePage.css';
 function HomePage() {
   return (
     <div className="home-page">
-      <h1>sewa vespa matic</h1>
-      <p>Temukan layanan terbaik kami yang dapat membantu meningkatkan bisnis Anda.</p>
+      <div className="home-page-content">
+        <div className="left-column">
+          <h1 className='home-page-title'>Jelajahi Keindahan Bandung dengan Gaya Retro!</h1>
+          <p>Nikmati sensasi berbeda mengelilingi kota Bandung dengan Vespa matic dari Pinas. Sempurna untuk wisata kota, berpetualang, atau sekadar bersantai, sewa Vespa kami menawarkan pengalaman tak terlupakan. Temukan spot-spot menarik dan ciptakan kenangan baru dengan berkendara penuh gaya. Pesan sekarang dan rasakan petualangan seru di Bandung!</p>
+        </div>
+        <div className="right-column">
+          {/* Kolom kanan dibiarkan kosong */}
+        </div>
+      </div>
     </div>
   );
 }
