dfba68d9e3ecc8c1b53ebd90a40b8472eca3244c
kurniawansaputraa <nebula.stars3122@gmail.com>
2024-06-26 15:38:25 +0700

    hoover pada navbar

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -9,6 +9,7 @@ function HomePage() {
         <div className="left-column">
           <h1 className='home-page-title'>Jelajahi Keindahan Bandung dengan Gaya Retro!</h1>
           <p>Nikmati sensasi berbeda mengelilingi kota Bandung dengan Vespa matic dari Pinas. Sempurna untuk wisata kota, berpetualang, atau sekadar bersantai, sewa Vespa kami menawarkan pengalaman tak terlupakan. Temukan spot-spot menarik dan ciptakan kenangan baru dengan berkendara penuh gaya. Pesan sekarang dan rasakan petualangan seru di Bandung!</p>
+          <button className="button-sewa">Sewa</button>
         </div>
         <div className="right-column">
           {/* Kolom kanan dibiarkan kosong */}
